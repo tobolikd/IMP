c7fd695a3a26cc5dfcf6611bca059928cb71f7d4
David Tobolik <tobolikg@gmail.com>
2023-10-17 19:46:41 +0200

    feat: graph drawing

--- main/utils.c
+++ main/utils.c
@@ -36,55 +36,115 @@ void ssd1306_turn_on(dev_conf_t device) {
 
 void show_temperature_info(dev_conf_t display, dev_conf_t senzor) {
     static display_buff buff;
-
+    static graph_data_t graph = {
+        .data = {0},
+        .min = INT16_MAX, // needs to be overwritten by first value
+        .max = INT16_MIN,
+        .idx = 0,
+        .full = false};
     memset(buff, 0x00, DISPLAY_BUFF_SIZE);
     // get info
-    // update num
+    // TODO
+    static int16_t temperature = 18;
+    temperature++;
+    if (temperature > 50)
+        temperature = 2;
+
+    // set temp value
+    set_metric_value(&buff, temperature);
+
+    // set temp unit
+    uint8_t xPosStart = METRIC_UNIT_START_X;
+    write_to_buff(&buff, degree, xPosStart, METRIC_UNIT_START_PAGE,
+                  GLYPH_DEGREE_WIDTH,
+                  GLYPH_DEGREE_HEIGHT / DISPLAY_PAGE_HEIGHT);
+    xPosStart += GLYPH_DEGREE_WIDTH;
+    write_to_buff(&buff, letter16x24_C, xPosStart, METRIC_UNIT_START_PAGE,
+                  GLYPH_16x24_WIDTH, GLYPH_16x24_HEIGHT / DISPLAY_PAGE_HEIGHT);
+
     // update graph
-    // structs to buf
+    add_graph_data(&graph, temperature);
+    draw_graph(&buff, &graph);
+
     // display
-    //
-    static int num = 0;
-    num++;
-    num %= 100;
-
-    set_temperature(&buff, num);
-    draw_graph(&buff, NULL);
-
     if (ssd1306_write_buffer(display, buff) != ESP_OK)
         ESP_LOGE(tag, "failed to write buffer");
 }
-void show_humidity_info(dev_conf_t display, dev_conf_t senzor) {}
 
-void set_temperature(display_buff *buff, uint8_t temperature) {
-    // no support for temp greater than 99
-    if (temperature >= 100)
+void show_humidity_info(dev_conf_t display, dev_conf_t senzor) {
+    static display_buff buff;
+    static graph_data_t graph = {.data = {0},
+                                 .min = INT16_MAX,
+                                 .max = INT16_MIN,
+                                 .idx = 0,
+                                 .full = false};
+    memset(buff, 0x00, DISPLAY_BUFF_SIZE);
+
+    // get humidity
+    // TODO
+
+    // set humidity value
+    set_metric_value(&buff, 105);
+
+    // set humidity unit
+    write_to_buff(&buff, symbol16x24_percent, METRIC_UNIT_START_X,
+                  METRIC_UNIT_START_PAGE, GLYPH_16x24_WIDTH,
+                  GLYPH_16x24_HEIGHT / DISPLAY_PAGE_HEIGHT);
+
+    // update graph
+    draw_graph(&buff, &graph);
+
+    // display
+    if (ssd1306_write_buffer(display, buff) != ESP_OK)
+        ESP_LOGE(tag, "failed to write buffer");
+}
+
+void set_metric_value(display_buff *buff, uint32_t value) {
+    uint32_t xPosStart = METRIC_VALUE_START_X;
+    // no support for temp greater than 999
+    if (value >= 1000)
         ESP_LOGW(tag, "temperature greater than 100 °C, first digit clipped");
 
-    uint32_t xPosStart = TEMPERATURE_START_X;
-    // write decimal
-    if (temperature >= 10) {
-        write_to_buff(buff, num16x24[(temperature % 100) / 10], xPosStart,
-                      TEMPERATURE_START_PAGE, GLYPH_16x24_WIDTH,
+    if (value >= 100) {
+        write_to_buff(buff, num16x24[(value % 1000) / 100], xPosStart,
+                      METRIC_VALUE_START_PAGE, GLYPH_16x24_WIDTH,
                       GLYPH_16x24_HEIGHT / DISPLAY_PAGE_HEIGHT);
     }
 
     // move start to next digit
     xPosStart += GLYPH_16x24_WIDTH;
-    write_to_buff(buff, num16x24[temperature % 10], xPosStart,
-                  TEMPERATURE_START_PAGE, GLYPH_16x24_WIDTH,
+    if (value >= 10) {
+        write_to_buff(buff, num16x24[(value % 100) / 10], xPosStart,
+                      METRIC_VALUE_START_PAGE, GLYPH_16x24_WIDTH,
+                      GLYPH_16x24_HEIGHT / DISPLAY_PAGE_HEIGHT);
+    }
+
+    xPosStart += GLYPH_16x24_WIDTH;
+    write_to_buff(buff, num16x24[value % 10], xPosStart,
+                  METRIC_VALUE_START_PAGE, GLYPH_16x24_WIDTH,
                   GLYPH_16x24_HEIGHT / DISPLAY_PAGE_HEIGHT);
 
     xPosStart += GLYPH_16x24_WIDTH;
-    write_to_buff(buff, degree, xPosStart, TEMPERATURE_START_PAGE,
-                  GLYPH_DEGREE_WIDTH,
-                  GLYPH_DEGREE_HEIGHT / DISPLAY_PAGE_HEIGHT);
-
-    xPosStart += GLYPH_DEGREE_WIDTH;
-    write_to_buff(buff, letter16x24_C, xPosStart, TEMPERATURE_START_PAGE,
-                  GLYPH_16x24_WIDTH, GLYPH_16x24_HEIGHT / DISPLAY_PAGE_HEIGHT);
 }
-void draw_graph(display_buff *buff, graph_data data) {
+
+void add_graph_data(graph_data_t *data, int16_t newValue) {
+    if (newValue > data->max)
+        data->max = newValue;
+    if (newValue < data->min)
+        data->min = newValue;
+
+    if (data->full) {
+        memmove(&data->data[0], &data->data[1], (MAX_GRAPH_DATA - 1) * sizeof(int16_t));
+        data->data[MAX_GRAPH_DATA - 1] = newValue;
+        return;
+    }
+
+    data->data[data->idx++] = newValue;
+    if (data->idx >= MAX_GRAPH_DATA - 1)
+        data->full = true;
+}
+
+void draw_graph(display_buff *buff, graph_data_t *data) {
     // verical axis
     for (int i = GRAPH_AXIS_VERTICAL_START_Y; i < GRAPH_AXIS_VERTICAL_END_Y;
          i++)
@@ -94,10 +154,32 @@ void draw_graph(display_buff *buff, graph_data data) {
          i++)
         set_pixel(buff, i, GRAPH_AXIS_HORIZONTAL_Y);
 
+    int16_t data_range = ((data->max - data->min) < GRAPH_DATA_MIN_RANGE)
+                             ? GRAPH_DATA_MIN_RANGE
+                             : data->max - data->min;
+
+    int16_t lower_bound = data->min;
+    int16_t upper_bound = data->min + data_range;
+
     // set lower bound
-    write_to_buff(buff, num8x8[0], GRAPH_LOWER_BOUND_START_X,
+    write_to_buff(buff, num8x8[lower_bound % 10], GRAPH_LOWER_BOUND_START_X,
                   GRAPH_LOWER_BOUND_START_PAGE, GLYPH_8x8_WIDTH,
                   GLYPH_8x8_HEIGHT / DISPLAY_PAGE_HEIGHT);
+
+    // set upper bound
+    write_to_buff(buff, num8x8[(upper_bound % 100) / 10],
+                  GRAPH_UPPER_BOUND_START_X, GRAPH_UPPER_BOUND_START_PAGE,
+                  GLYPH_8x8_WIDTH, GLYPH_8x8_HEIGHT / DISPLAY_PAGE_HEIGHT);
+    write_to_buff(buff, num8x8[upper_bound % 10],
+                  GRAPH_UPPER_BOUND_START_X + GLYPH_8x8_WIDTH,
+                  GRAPH_UPPER_BOUND_START_PAGE, GLYPH_8x8_WIDTH,
+                  GLYPH_8x8_HEIGHT / DISPLAY_PAGE_HEIGHT);
+
+    for (uint8_t i = 0; i <= data->idx; i++) {
+        set_pixel(buff, GRAPH_DATA_START_X + i,
+                  GRAPH_DATA_START_Y - ((data->data[i] - lower_bound) *
+                                        GRAPH_POINT_MAX_RANGE / data_range));
+    }
 }
 
 void write_to_buff(display_buff *buff, const uint8_t *data, uint32_t xPos,
--- main/utils.h
+++ main/utils.h
@@ -3,9 +3,9 @@
 
 #include "i2c-utils.h"
 #include "ssd1306.h"
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
-#include <sys/types.h>
 
 // graphic utils
 #define LINE_VERTICAL 0x80
@@ -13,6 +13,7 @@
 
 #define LINE_HORIZONTAL_BOTTOM 0x80
 
+// graph position
 #define GRAPH_AXIS_VERTICAL_X 16
 #define GRAPH_AXIS_VERTICAL_START_Y 24
 #define GRAPH_AXIS_VERTICAL_END_Y 60
@@ -20,17 +21,32 @@
 #define GRAPH_AXIS_HORIZONTAL_Y 55
 #define GRAPH_AXIS_HORIZONTAL_START_X 12
 #define GRAPH_AXIS_HORIZONTAL_END_X 96
+
 #define GRAPH_LOWER_BOUND_START_X 8
 #define GRAPH_LOWER_BOUND_START_PAGE 7
 #define GRAPH_UPPER_BOUND_START_X 0
 #define GRAPH_UPPER_BOUND_START_PAGE 3
 
-#define TEMPERATURE_START_PAGE 0
-#define TEMPERATURE_START_X 64
+// metric position
+#define METRIC_VALUE_START_PAGE 0
+#define METRIC_VALUE_START_X 48
+#define METRIC_UNIT_START_PAGE 0
+#define METRIC_UNIT_START_X 96
 
+#define GRAPH_DATA_START_X 17
+#define GRAPH_DATA_START_Y 54
 #define MAX_GRAPH_DATA 80
-typedef uint8_t graph_data[MAX_GRAPH_DATA];
+#define GRAPH_DATA_MIN_RANGE 10
+#define GRAPH_POINT_MAX_RANGE 30
+typedef struct GraphData {
+    int16_t data[MAX_GRAPH_DATA];
+    int16_t min;
+    int16_t max;
+    uint8_t idx;
+    bool full;
+} graph_data_t;
 
+void add_graph_data(graph_data_t *data, int16_t newValue);
 
 /**
  * initialize ssd1306 display settings
@@ -41,8 +57,8 @@ void write_to_buff(display_buff *buff, const uint8_t *data, uint32_t xPos,
                    uint32_t page, uint32_t width, uint32_t pageCount);
 void set_pixel(display_buff *buff, uint8_t x, uint8_t y);
 
-void set_temperature(display_buff *buff, uint8_t temperature);
-void draw_graph(display_buff *buff, graph_data data);
+void set_metric_value(display_buff *buff, uint32_t value);
+void draw_graph(display_buff *buff, graph_data_t *data);
 
 void show_temperature_info(dev_conf_t display, dev_conf_t senzor);
 void show_humidity_info(dev_conf_t display, dev_conf_t senzor);
